57cee7ba77ba41d47ae0573670f366288b26ea0e
Karlo Pintaric <96391450+karlopintaric@users.noreply.github.com>
2025-02-23 18:52:51 +0100

    Add description to header

--- src/ui/header/Header.jsx
+++ src/ui/header/Header.jsx
@@ -2,12 +2,18 @@ import "./Header.css";
 
 export default function Header({ score, bestScore }) {
   return (
-    <header>
-      <h1>Pokemon Memory Game</h1>
-      <div className="score">
-        <p>Score: {score}</p>
-        <p>Best Score: {bestScore}</p>
-      </div>
-    </header>
+    <>
+      <header>
+        <h1>Pokemon Memory Game</h1>
+        <div className="score">
+          <p>Score: {score}</p>
+          <p>Best Score: {bestScore}</p>
+        </div>
+      </header>
+      <p>
+        Get points by clicking on an image but don't click on any more than
+        once!
+      </p>
+    </>
   );
 }
